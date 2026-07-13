30e6173cad7cab2c29a078a8fc050ad951f9c76e
Wellington Herrera <12554647+WtonFox@users.noreply.github.com>
2026-07-13 12:41:55 -0400

    fix: quitar bg del overlay del modal

--- components/IndicatorModal.tsx
+++ components/IndicatorModal.tsx
@@ -548,7 +548,7 @@ export const IndicatorModal: React.FC<IndicatorModalProps> = ({
 
   return (
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 p-4"
+      className="fixed inset-0 z-50 flex items-center justify-center p-4"
       onClick={handleBackdrop}
     >
       <div
